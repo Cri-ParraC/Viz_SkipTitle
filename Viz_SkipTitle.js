11aafcb99d9aeb785a7639281666d6d6295fd8b9
Cri-ParraC <cri.parrac@gmail.com>
2024-06-25 19:28:53 -0400

    Actualizada sección de ayuda

--- Viz_SkipTitle.js
+++ Viz_SkipTitle.js
@@ -1,22 +1,25 @@
 //=============================================================================
-// Viz_SkipTitle.js [MZ] (v1.0.0)
+// Viz_SkipTitle.js [MZ] (v1.0.1)
 //=============================================================================
 
 /*:
  * @target MZ
- * @plugindesc [MZ] (v1.0.0) Salta la pantalla de título e inicia una nueva partida.
+ * @plugindesc [MZ] (v1.0.1) Salta la pantalla de título e inicia una nueva partida.
  * @author Vizcacha
- * @version 1.0.0
- * @url https://github.com/Cri-ParraC/
- * @help Viz_SkipTitle.js [MZ] (v1.0.0)
- * 
- * Plugin sencillo para RPG Maker MZ que se salta la pantalla de título e inicia
+ * @version 1.0.1
+ * @url https://github.com/Cri-ParraC/Viz_SkipTitle
+ * @help Viz_SkipTitle.js [MZ] (v1.0.1)
+ * ----------------------------------------------------------------------------
+ * Plugin sencillo para RPG Maker MZ que salta la pantalla de título e inicia
  * una nueva partida. Ideal para pruebas rápidas.
+ * 
+ * Se puede lograr lo mismo activando una opción desde el editor de RPG Maker,
+ * en "Juego" => "Omitir pantalla de título".
  */
 
 (() => {
   "use strict";
-  console.info("Viz_SkipTitle.js [MZ] (v1.0.0) activado");
+  console.info("Viz_SkipTitle.js [MZ] (v1.0.1) activado");
 
   Scene_Boot.prototype.startNormalGame = function () {
     this.checkPlayerLocation();
